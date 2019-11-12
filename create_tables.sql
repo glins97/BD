d18c7c1938632b6787c1f723e2bbb584eb6dd1da
Gabriel Lins <gabriel.lins97@gmail.com>
2019-11-12 16:28:28 -0300

    Table 'items' renamed to 'subvendas'; fields in 'clients' are now nullable

--- create_tables.sql
+++ create_tables.sql
@@ -8,6 +8,7 @@ CREATE TABLE produtos(
 CREATE TABLE materiais(
     material_id int(16) NOT NULL,
     produto_id int(16) NOT NULL,
+    produto_qtd int(16) NOT NULL,
     PRIMARY KEY (material_id),
     FOREIGN KEY (produto_id) REFERENCES produtos(produto_id)
 );
@@ -29,9 +30,9 @@ CREATE TABLE pontos_venda(
 CREATE TABLE clientes(
     cpf varchar(14) NOT NULL,
     nome varchar(140) NOT NULL,
-    email varchar(140) NOT NULL,
-    telefone varchar(140) NOT NULL,
-    endereco varchar(140) NOT NULL,
+    email varchar(140),
+    telefone varchar(140),
+    endereco varchar(140),
     PRIMARY KEY (cpf)
 );
 
@@ -45,12 +46,12 @@ CREATE TABLE vendas(
     FOREIGN KEY (cpf) REFERENCES clientes(cpf)
 );
 
-CREATE TABLE items(
-    item_id int(16) NOT NULL,
+CREATE TABLE subvenda(
+    subvenda_id int(16) NOT NULL,
     valor_total float(32) NOT NULL,
     produto_id int(16) NOT NULL,
     venda_id int(16) NOT NULL,
-    PRIMARY KEY (item_id),
+    PRIMARY KEY (subvenda_id),
     FOREIGN KEY (produto_id) REFERENCES produtos(produto_id),
     FOREIGN KEY (venda_id) REFERENCES vendas(venda_id)
 );
