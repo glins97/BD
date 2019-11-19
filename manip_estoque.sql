e118fcd2add0bc328bc796eb5a5ea7014e496b53
Ingrid Lorraine Rodrigues da Silva <ingridsilva71@gmail.com>
2019-11-19 16:56:50 -0200

    Adding Inventory Update Commands

--- manip_estoque.sql
+++ manip_estoque.sql
@@ -1,4 +1,14 @@
-
 -- Adicionar estoque
+INSERT INTO estoque
+    (material_id, quantidade)
+VALUES 
+    (-1, -1);
+
 -- Remover estoque
+ DELETE FROM estoque WHERE material_id = '' ;
+
 -- Atualizar estoque
+
+UPDATE estoque
+SET quantidade = -1
+WHERE quantidade = -1
