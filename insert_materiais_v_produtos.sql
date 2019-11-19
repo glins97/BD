66ffeec302e518ae2602d208ecde26e375a54fa4
Gabriel Lins <gabriel.lins97@gmail.com>
2019-11-19 16:55:53 -0300

    Comments on 'materiais_v_produtos'

--- insert_materiais_v_produtos.sql
+++ insert_materiais_v_produtos.sql
@@ -1,58 +1,70 @@
+-- Kit1 tem 500g de Café L1 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (1, 1, 500);
 
+-- Kit1 tem 1 Moedor 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (1, 3, 1);
 
+-- Kit1 tem 3 Eco-Capsulas 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (1, 4, 3);
 
+-- Kit2 tem 500g de Café L1 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (2, 1, 500);
 
+-- Kit2 tem 1 Moedor 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (2, 3, 1);
 
+-- Kit2 tem 6 Eco-Capsulas 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (2, 4, 6);
 
+-- Lote1 tem 500g Café L1 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (3, 1, 500);
 
+-- Lote2 tem 500g Café L1 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (4, 2, 500);
 
+-- Melange tem 250g Café L1 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (5, 1, 250);
 
+-- Melange tem 250g Café L2 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (5, 2, 250);
 
+-- Kit3 tem 500g L1 (e subproduto Kit1) 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
     (6, 1, 500);
 
+-- Kit4 tem 500g L2 (e subproduto Kit2) 
 INSERT INTO materiais_v_produtos
     (produto_id, material_id, qtd)
 VALUES 
